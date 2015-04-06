b301113e88624dc8389e510f1d3160828352affc
Chrestella <chres_tella08@yahoo.com>
2015-04-06 20:54:26 +0700

    force push 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,10 +50,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      <ul class="menu nav navbar-nav ">
 		        <li class="active"><a href="index.html">home</a></li>
-		        <li><a href="feature.html">features</a></li>
-		        <li><a href="blog.html">blog</a></li>
-		        <li><a href="about.html">about</a></li>
-		        <li><a href="contact.html">contact</a></li>
+		         <li><a href="feature.html">profil user</a></li>
+		        <!-- <li><a href="blog.html">STPW</a></li>
+		        <li><a href="about.html">ITPMB</a></li> 
+		        <li><a href="contact.html">contact</a></li> -->
 		      </ul>
 		      <form class="navbar-form navbar-right" role="search">
 		        <div class="form-group my_search">
