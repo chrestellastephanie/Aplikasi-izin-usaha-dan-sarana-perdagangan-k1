436c6fb109d37026867acdb0b9e17d72e1ed3c1a
Chrestella <chres_tella08@yahoo.com>
2015-04-06 21:55:34 +0700

    permohonan/view

--- resources/views/permohonan/lihatIzin.blade.php
+++ resources/views/permohonan/lihatIzin.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 	<h1>Daftar permohonan : </h1>
-	<h2>IUTM-IUPP-IUPPT</h2>
+	<h3>IUTM-IUPP-IUPPT</h3>
 	<ul>
 	@foreach ($iutm as $mohon)
+		
 		<li>
 			{{ $mohon->namaPerusahaan }}
 		</li>
+		
 	@endforeach
 	</ul>
 	<h2>STPW</h2>
