409e93d041ac8eb0fc28752c970edb64693cb346
Chrestella <chres_tella08@yahoo.com>
2015-04-06 22:32:37 +0700

    view udh ada button

--- resources/views/permohonan/lihatIzin.blade.php
+++ resources/views/permohonan/lihatIzin.blade.php
@@ -5,27 +5,76 @@
 	<h3>IUTM-IUPP-IUPPT</h3>
 	<ul>
 	@foreach ($iutm as $mohon)
-		
-		<li>
-			{{ $mohon->namaPerusahaan }}
-		</li>
-		
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="col-md-8">
+					<li>
+						<h4>{{ $mohon->namaPerusahaan }}</h4>
+					</li>
+					<li>
+						{{ $mohon->waktuPengajuan }}
+					</li>
+				</div>
+				<div class="col-md-4">
+					<button class="btn2 btn_style">cek berkas</button> |
+					<select>
+						<option>Accept</option>
+						<option>Decline</option>
+					</select>
+					<button class="btn2 btn_style">ubah status</button>
+				</div>
+			</div>
+		</div>
 	@endforeach
 	</ul>
 	<h2>STPW</h2>
 	<ul>
 	@foreach ($stpw as $mohon)
-		<li>
-			{{ $mohon->namaPerusahaan }}
-		</li>
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="col-md-8">
+					<li>
+						<h4>{{ $mohon->namaPerusahaan }}</h4>
+					</li>
+					<li>
+						{{ $mohon->waktuPengajuan }}
+					</li>
+				</div>
+				<div class="col-md-4">
+					<button class="btn2 btn_style">cek berkas</button> |
+					<select>
+						<option>Accept</option>
+						<option>Decline</option>
+					</select>
+					<button class="btn2 btn_style">ubah status</button>
+				</div>
+			</div>
+		</div>
 	@endforeach
 	</ul>
 	<h2>ITPMB</h2>
 	<ul>
 	@foreach ($itpmb as $mohon)
-		<li>
-			{{ $mohon->namaPerusahaan }}
-		</li>
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="col-md-8">
+					<li>
+						<h4>{{ $mohon->namaPerusahaan }}</h4>
+					</li>
+					<li>
+						{{ $mohon->waktuPengajuan }}
+					</li>
+				</div>
+				<div class="col-md-4">
+					<button class="btn2 btn_style">cek berkas</button> |
+					<select>
+						<option>Accept</option>
+						<option>Decline</option>
+					</select>
+					<button class="btn2 btn_style">ubah status</button>
+				</div>
+			</div>
+		</div>
 	@endforeach
 	</ul>
 @endsection
