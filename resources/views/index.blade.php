6018a58fd2fcac0742c084663b65ab37e1250241
Chrestella <chres_tella08@yahoo.com>
2015-05-15 12:25:40 +0700

    button perpanjang izin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,14 +70,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="row slider text-center">
 			<div class="col-md-8">
 					<div class="col-md-10 slider_text">
-						<h2>Aplikasi Izin Usaha dan Sarana Perdagangan</h2>
-						<h3>Kota Bandung</h3><br>
-						<a href="#pilihanIzin"><button class="btn_style">Ajukan Izin</button></a>
+						<h2>Mau buat usaha?</h2>
+						<h3>Daftar dulu yuk!</h3><br>
+						<a href="#pilihanIzin"><button class="btn_style">Ajukan Izin</button></a> &nbsp;
+						<a href="user-profile"><button class="btn_style">Perpanjang izin</button></a>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="slider_img">
-						<img src="/images/pic1.png" alt="" class="img-responsive"/>
+						<img src="{{ asset('images/pic1.png') }}" alt="" class="img-responsive"/>
 					</div>
 				</div>
 			</div>
@@ -174,13 +175,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="row  footer">
 			<div class="col-md-3 span1_of_4">
 				<h4>about us</h4>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry .....</p>
+				<p><b>Aplikasi Izin Usaha dan Sarana Perdagangan</b>
+					<br>
+					Aplikasi ini dibuat untuk mempermudah warga Kota Bandung untuk mengajukan permohonan izin usaha dan sarana perdagangan. Aplikasi ini mengurangi jumlah berkas yang harus diberikan pemohon dan mempersingkat jalur birokrasi.
+				</p>
 				<h5>Address</h5>
-				<p class="top">500 Lorem Ipsum Dolor Sit,</p>
-				<p>22-56-2-9 Sit Amet,</p>
-				<p>USA</p>
-				<p>Phone:(00) 222 666 444</p>
-				<p>Fax: (000) 000 00 00 0</p>
+				<p class="top">Dinas Koperasi, UKM dan Perindustrian Perdagangan</p>
+				<p>Jl. Kawaluyaan No. 2 Bandung</p>
+				<p>Phone:(022) 730 83 58</p>
+				<p>Fax: (022) 730 83 58</p>
 			</div>
 			<div class="col-md-3 span1_of_4">
 				<h4>latest posts</h4>
