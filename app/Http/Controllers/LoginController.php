ee0ce7cfce6ab8ee35dec44eef6a1aa1752e5015
ClearingPath <13512093@std.stei.itb.ac.id>
2015-05-16 22:00:55 +0700

    edit data

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class LoginController extends Controller {
 
 	public function UserLogin(){
-        return view(login.login);
+        return view('login.login');
     }
 
 }
