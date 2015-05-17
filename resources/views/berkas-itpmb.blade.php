23cd53588a34015236d5b4b230578f9bac3a3856
Chrestella <chres_tella08@yahoo.com>
2015-05-17 20:19:56 +0700

    minor

--- resources/views/berkas-itpmb.blade.php
+++ resources/views/berkas-itpmb.blade.php
@@ -36,7 +36,7 @@
 			<tr>
 				<td style="width:1%;"><input type="checkbox" name="KepemilikanTempatValid" value="valid"></td>
 				<td style="width:20%;">IMB</td>
-				<td style="width:40%;">cek dari yang lain</td>
+				<td style="width:40%;">-</td>
 			</tr>
 			<tr>
 				<td style="width:1%;"><input type="checkbox" name="KepemilikanTempatValid" value="valid"></td>
@@ -72,11 +72,6 @@
 				<td style="width:1%;"><input type="checkbox" name="KepemilikanTempatValid" value="valid"></td>
 				<td style="width:20%;">Miras Gol C</td>
 				<td style="width:40%;">{{$permohonan->miras_golC}}</td>
-			</tr>
-			<tr>
-				<td style="width:1%;"><input type="checkbox" name="aktaPendirianValid" value="valid"></td>
-				<td style="width:15%;">Akta Pendirian Perusahaan</td>
-				<td style="width:40%;"><a href="/iusp/public/images/uploaded-image/{{$permohonan->aktaPendirianPerusahaan}}">{{$permohonan->aktaPendirianPerusahaan}}</a></td>
 			</tr>
 		</table><br>
 		<button form="setuju" class="btn btn_style buttonSetuju">Setuju</button> <button form="tolak" class="btn btn_style btnTolak">Tolak</button>
--- resources/views/berkas-stpw.blade.php
+++ resources/views/berkas-stpw.blade.php
@@ -35,7 +35,7 @@
 			<tr>
 				<td style="width:1%;"><input type="checkbox" name="KepemilikanTempatValid" value="valid"></td>
 				<td style="width:20%;">IMB</td>
-				<td style="width:40%;">cek dari yang lain</td>
+				<td style="width:40%;">-</td>
 			</tr>
 			<tr>
 				<td style="width:1%;"><input type="checkbox" name="KepemilikanTempatValid" value="valid"></td>
