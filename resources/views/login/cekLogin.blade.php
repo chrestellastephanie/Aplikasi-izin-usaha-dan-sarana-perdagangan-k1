8d35f2b3cacf0f49fb625b20ad6bbdbd2330edc6
ClearingPath <13512093@std.stei.itb.ac.id>
2015-05-17 07:36:07 +0700

    edit login url

--- resources/views/login/cekLogin.blade.php
+++ resources/views/login/cekLogin.blade.php
@@ -16,7 +16,7 @@
                     var url = "{{url()}}/home?id="+data;
                     window.location.href = url;
                 } else { //redirect ke alamat login kalian
-                    var url = "{{url()}}/home"
+                    var url = "{{url()}}/login"
                     window.location.href = url
                 }
             },
