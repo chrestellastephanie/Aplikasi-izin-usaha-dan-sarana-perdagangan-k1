bfba42d2f677c3a7556adda819d797ec87a76113
Chrestella <chres_tella08@yahoo.com>
2015-05-17 09:50:17 +0700

    get id login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <body>
 	<!-- get id yang login -->
 	<?php $idlogin= $_GET['id']
-		echo '<h1>'.$idlogin.'</h1>';
+		echo "<h1>".$idlogin."</h1>";
 
 	?>
 
